2c4dd759357ca96967e9eb61c23a4e4d09862190
Stevan Olegario <stevan@poise.local>
2018-05-22 17:31:29 -0300

    health_check_config

---
+++ config/initializers/health_check.rb
@@ -0,0 +1,33 @@
+HealthCheck.setup do |config|
+
+  # uri prefix
+  config.uri = 'v1/ping'
+
+  # Text output upon success
+  config.success = 'API is up and running'
+
+  # Timeout in seconds used when checking smtp server
+  config.smtp_timeout = 30.0
+
+  # http status code used when plain text error message is output
+  # Set to 200 if you want your want to distinguish between partial (text does not include success) and
+  # total failure of rails application (http status of 500 etc)
+
+  config.http_status_for_error_text = 500
+
+  # http status code used when an error object is output (json or xml)
+  # Set to 200 if you want your want to distinguish between partial (healthy property == false) and
+  # total failure of rails application (http status of 500 etc)
+
+  config.http_status_for_error_object = 500
+
+
+  # Or to exclude one check:
+  config.standard_checks -= [ 'emailconf', 'database', 'migrations' ]
+
+  # max-age of response in seconds
+  # cache-control is public when max_age > 1 and basic_auth_username is not set
+  # You can force private without authentication for longer max_age by
+  # setting basic_auth_username but not basic_auth_password
+  config.max_age = 1
+end
