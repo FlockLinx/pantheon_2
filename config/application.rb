259754bd4d263460f18e073614f8985e561fedaa
Stevan Olegario <stevan@poise.local>
2018-08-17 19:37:23 +0100

    add expose headers

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Pantheon2
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options, :head]
+        resource '*', headers: :any, methods: %i[get post put patch delete options head], expose: ['ETag', 'access-token', 'client', 'uid']
       end
     end
   end
