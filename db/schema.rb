ea7213e9f19fb46519949888f9e2deb5bfafa027
Stevan olegario <stevan.olegario@gmail.com>
2019-06-11 19:42:53 -0300

    a new begin

--- db/schema.rb
+++ db/schema.rb
@@ -144,7 +144,7 @@ ActiveRecord::Schema.define(version: 2018_06_12_142217) do
     t.string "secondary_email"
     t.integer "created_by_user_id"
     t.boolean "active", default: false
-    t.integer "role", default: 0
+    t.string "role"
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
