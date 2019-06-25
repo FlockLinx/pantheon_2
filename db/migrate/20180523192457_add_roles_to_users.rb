eff1f02dbe92c66f860b6c2f2982226a2dd4ecd6
Stevan olegario <stevan.olegario@gmail.com>
2019-06-25 18:45:01 -0300

    trying to reactivate my side prog part 2

--- db/migrate/20180523192457_add_roles_to_users.rb
+++ db/migrate/20180523192457_add_roles_to_users.rb
@@ -1,5 +1,5 @@
 class AddRolesToUsers < ActiveRecord::Migration[5.2]
   def change
-    # change_column :users, :role, 'integer USING CAST(role AS integer', default: 0
+    change_column :users, :role, 'integer USING CAST(role AS integer', default: 0
   end
 end
