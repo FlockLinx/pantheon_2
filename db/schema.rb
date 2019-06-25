7372c77271ace807aed9547c5a44910bfda221d1
Stevan olegario <stevan.olegario@gmail.com>
2019-06-25 18:47:58 -0300

    omg i commited schema

--- db/schema.rb
+++ db/schema.rb
@@ -144,7 +144,7 @@ ActiveRecord::Schema.define(version: 2018_06_12_142217) do
     t.string "secondary_email"
     t.integer "created_by_user_id"
     t.boolean "active", default: false
-    t.string "role"
+    t.integer "role", default: 0
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
